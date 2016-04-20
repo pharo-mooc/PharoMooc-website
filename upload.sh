c759e6d98002a4887972f2fefbe292eb79927f29
Damien Cassou <damien@cassou.me>
2016-04-20 18:09:02 +0200

    Use rsync for upload script

--- upload.sh
+++ upload.sh
@@ -1,10 +1,5 @@
 #!/bin/sh
 
-cd ..
 echo Uploading files to files.pharo.org...
-scp -r mooc files.pharo.org: 2>&1 | grep -v '.git'
-echo uploading done
-echo Moving files to webserver
-ssh files.pharo.org sudo -u filepharosync cp -R ~/mooc /appli/files.pharo.org/ 2>&1 | grep -v '.git'
-echo moving done
-echo success
+rsync --progress --cvs-exclude --recursive --links --times --delete . filepharosync@files.pharo.org:/appli/files.pharo.org/mooc
+echo done
