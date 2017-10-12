233a4e42733ff4a3e8681c3045c53c6c9d75996b
StephaneDucasse <stephane.ducasse@inria.fr>
2017-10-12 15:15:01 +0200

    remove delete

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
 echo Uploading files to files.pharo.org...
-rsync --progress --cvs-exclude --recursive --links --times --delete . filepharosync@files.pharo.org:/appli/files.pharo.org/mooc
+rsync --progress --cvs-exclude --recursive --links --times . filepharosync@files.pharo.org:/appli/files.pharo.org/mooc
 echo done
