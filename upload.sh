3ff1fc5f4ad2bb7781078603e443e41b7cf79dd2
StephaneDucasse <stephane.ducasse@inria.fr>
2017-12-10 16:20:18 +0100

    updating to new servers

--- upload.sh
+++ upload.sh
@@ -1,5 +1,9 @@
 #!/bin/sh
 
-echo Uploading files to files.pharo.org...
-rsync --progress --cvs-exclude --recursive --links --times . filepharosync@files.pharo.org:/appli/files.pharo.org/mooc
+#echo Uploading files to files.pharo.org...
+#rsync --progress --cvs-exclude --recursive --links --times . #filepharosync@files.pharo.org:/appli/files.pharo.org/mooc
+#echo done
+
+echo Uploading files to files.pharo OVH!...
+rsync --progress --cvs-exclude --recursive --links --times . pharoorgde@files.pharo:mooc
 echo done
